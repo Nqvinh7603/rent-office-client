44a01177af25dd814ecbe5676750217172b68e7f
Nqvinh7603 <nqvinh7603@gmail.com>
2025-04-26 13:04:42 +0700

    updaate code

--- src/features/office/compoents/office-detail/OfficeHeader.tsx
+++ src/features/office/compoents/office-detail/OfficeHeader.tsx
@@ -1,4 +1,3 @@
-import { Rate } from "antd";
 import React from "react";
 
 interface OfficeHeaderProps {
@@ -20,7 +19,7 @@ const OfficeHeader: React.FC<OfficeHeaderProps> = ({
         <h1 className="mb-2 text-3xl font-bold text-gray-900">{name}</h1>
         <p className="text-gray-500">{address}</p>
       </div>
-      <div className="mt-4 flex items-center lg:mt-0">
+      {/* <div className="mt-4 flex items-center lg:mt-0">
         <Rate
           allowHalf
           defaultValue={stars}
@@ -28,7 +27,7 @@ const OfficeHeader: React.FC<OfficeHeaderProps> = ({
           className="text-yellow-500"
         />
         <span className="ml-2 text-sm text-gray-500">{rating} đánh giá</span>
-      </div>
+      </div> */}
     </div>
   );
 };
